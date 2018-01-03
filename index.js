f4a5fa44984e39a1ae2c62a57f321055a692751d
Nidit <github@nidit.cl>
2018-01-03 19:52:53 -0300

    Create css file for each shape and minify

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
-var
-  icons2font      = require('svgicons2svgfont'),
-  fs              = require('fs'),
-  path            = require('path'),
-  svg2ttf         = require('svg2ttf'),
-  ttf2eot         = require('ttf2eot'),
-  ttf2woff        = require('ttf2woff'),
-  args            = require('yargs').argv;
+var fs = require('fs');
+var path = require('path');
+var args = require('yargs').argv;
+var svg2ttf = require('svg2ttf');
+var ttf2eot = require('ttf2eot');
+var ttf2woff = require('ttf2woff');
+var CleanCSS = require('clean-css');
+var icons2font = require('svgicons2svgfont');
+
+function bufferFrom(source) {
+    return typeof Buffer.from === 'function' ? Buffer.from(source) : new Buffer(source);
+}
+
+function logError(err) {
+    if (err) {
+        console.error(err);
+    }
+}
+
 /**
  * We need to loop through the unicode character list and pass them all for svgicons2svgfont.
  * If you want to reduce the file-sizes with the cost of worse character support, you can
@@ -17,73 +28,80 @@ var
  * tff and woff versions.
  * @param {number=} [max=767]
  */
+
 var MAX_VALUE = args.max || 767;
+
 /**
  * Comma-separated list of the supported shapes.
  * @param {string} [shapes='circle,square,disc']
  */
+
 var SHAPES = args.shapes || 'circle,square,disc';
 
-var styleTemplate = fs.readFileSync('style-template.css', 'utf-8'), stylesheet = '',
-  characters = [];
+var DIST_DIR = path.join(__dirname, 'dist');
+var ASSETS_DIR = path.join(__dirname, 'assets');
+
+var styleTemplate = fs.readFileSync('style-template.css', 'utf-8');
+var fullStylesheet = '';
+var cssOptions = { level: 2 };
+var characters = [];
 
 for (var i = 0; i <= MAX_VALUE; i++){
   characters.push(String.fromCharCode(i));
 }
 
-function bufferFrom(source) {
-  return typeof Buffer.from === 'function' ? Buffer.from(source) : new Buffer(source);
-}
-
-function logError(err) {
-  if (err) {
-    console.error(err);
-  }
-}
-
-var DIST_DIR = path.join(__dirname, 'dist');
-
 if (!fs.existsSync(DIST_DIR)) {
-  fs.mkdirSync(DIST_DIR);
-} 
+    fs.mkdirSync(DIST_DIR);
+}
 
-SHAPES.split(',').forEach(function (shape) {
-  var
-    fontName = 'text-security-' + shape,
-    fontPath = path.join(DIST_DIR, fontName),
-    fontStream = icons2font({
-      fontName: fontName
+SHAPES.split(',').forEach(function (s) {
+    var shape = s.trim();
+
+    var fontName = 'text-security-' + shape;
+    var fontPath = path.join(DIST_DIR, fontName);
+    var fontStream = new icons2font({
+        fontName: fontName,
+        fontHeight: 2000,
+        normalize: true
     });
 
-  fontStream
-    .pipe(fs.createWriteStream(fontPath + '.svg'))//Create the .svg font
-    .on('finish', function () {
-      //Create the other formats using the newly created font and Fontello's conversion libs
+    fontStream
+        .pipe(fs.createWriteStream(fontPath + '.svg')) //Create the .svg font
+        .on('finish', function () {
+            // Create the other formats using the newly created font and Fontello's conversion libs
 
-      var ttf = svg2ttf(fs.readFileSync(fontPath + '.svg', 'utf-8'), {});
-      fs.writeFileSync(fontPath + '.ttf', bufferFrom(ttf.buffer), 'utf-8');
+            var ttf = svg2ttf(fs.readFileSync(fontPath + '.svg', 'utf-8'), {});
+            fs.writeFileSync(fontPath + '.ttf', bufferFrom(ttf.buffer), 'utf-8');
 
-      // ttf2eot and ttf2woff expect a buffer, while svg2ttf seems to expect a string
-      // this would be better read from the buffer, but will do for now
-      var ttfFile = fs.readFileSync(fontPath + '.ttf');
+            // ttf2eot and ttf2woff expect a buffer, while svg2ttf seems to expect a string
+            // this would be better read from the buffer, but will do for now
+            var ttfFile = fs.readFileSync(fontPath + '.ttf');
 
-      var eot = ttf2eot(ttfFile, {});
-      fs.writeFile(fontPath + '.eot', bufferFrom(eot.buffer), 'utf-8', logError);
+            var eot = ttf2eot(ttfFile, {});
+            fs.writeFile(fontPath + '.eot', bufferFrom(eot.buffer), 'utf-8', logError);
 
-      var woff = ttf2woff(ttfFile, {});
-      fs.writeFile(fontPath + '.woff', bufferFrom(woff.buffer), 'utf-8', logError);
-    })
-    .on('error', logError);
+            var woff = ttf2woff(ttfFile, {});
+            fs.writeFile(fontPath + '.woff', bufferFrom(woff.buffer), 'utf-8', logError);
+        })
+        .on('error', logError);
 
-  var glyph = fs.createReadStream(path.join(__dirname, 'assets', shape + '.svg'));
-  glyph.metadata = {
-    unicode: characters,
-    name: shape
-  };
-  fontStream.write(glyph);
-  fontStream.end();
-  //Append the new shape to the generated stylesheet
-  stylesheet += styleTemplate.replace(/\{\{shape}}/g, shape) + '\n';
+    var glyph = fs.createReadStream(path.join(ASSETS_DIR, shape + '.svg'));
+    glyph.metadata = {
+        unicode: characters,
+        name: shape
+    };
+
+    fontStream.write(glyph);
+    fontStream.end();
+
+    var stylesheet = styleTemplate.replace(/{{shape}}/g, shape);
+    var minStylesheet = new CleanCSS(cssOptions).minify(stylesheet);
+
+    fs.writeFile(path.join(DIST_DIR, fontName + '.css'), stylesheet, logError);
+    fs.writeFile(path.join(DIST_DIR, fontName + '.min.css'), minStylesheet.styles, logError);
+
+    fullStylesheet +=  stylesheet + '\n';
 });
-
-fs.writeFile(path.join(__dirname, 'dist', 'text-security.css'), stylesheet, logError);
+var minFullStylesheet = new CleanCSS(cssOptions).minify(fullStylesheet);
+fs.writeFile(path.join(DIST_DIR, 'text-security.css'), fullStylesheet, logError);
+fs.writeFile(path.join(DIST_DIR, 'text-security.min.css'), minFullStylesheet.styles, logError);
