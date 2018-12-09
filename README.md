fa960d4d964e00b5140ac7c25bf8cda96e997968
Oskari Noppa <the.nobba@gmail.com>
2018-12-10 01:53:58 +0200

    Fix #4 - Add wider unicode support
    Construct a ttx file manually, so that we can fit a lot more unicode
    points in the generated font.
    
    Also lots of other code style changes/refactoring.

--- README.md
+++ README.md
@@ -19,9 +19,14 @@ fact, the project was created for this exact purpose [as an answer to a
 StackOverflow
 question](https://stackoverflow.com/questions/36935576/how-to-make-input-type-tel-work-as-type-password/36950075#36950075).
 
+## Version 3 Beta
+You are looking at the branch for version 3, which is currently in alpha. Not everything works yet
+and things will break later. On the bright side, [issue #4](https://github.com/noppa/text-security/issues/4)
+is fixed in this branch.
+
 ## Installation
 ```
-npm install text-security
+npm install text-security@3-alpha.0
 ```
 
 You can use the fonts by adding this repo as a dependency and including
---
+++ build.js
@@ -0,0 +1,127 @@
+const fs = require('fs')
+const childProcess = require('child_process')
+const {promisify} = require('util')
+const SvgPath = require('svgpath')
+const svg = require('svg2ttf/lib/svg')
+const sfnt = require('svg2ttf/lib/sfnt')
+const svgson = require('svgson')
+
+const codepoints = 0x10f7be
+const names = ['disc']
+const br = '\n'
+const readFile = promisify(fs.readFile)
+const writeFile = promisify(fs.writeFile)
+const joinWith = (list, fn) => list.map(fn).join('')
+const tmpDir = './tmp'
+const distDir = './bin'
+
+for (const dir of [tmpDir, distDir]) {
+	if (!fs.existsSync(dir)) {
+		fs.mkdirSync(dir)
+	}
+}
+
+const promises = names.map(async name => {
+	const results = []
+	for (let i = 0; i < codepoints; i++) {
+		results.push(
+			`<map code="0x${i.toString(16)}" name="${name}" />`
+		)
+	}
+
+	const svgInfo = await readFile(`./assets/${name}.svg`, 'utf8').then(svgson.parse)
+	const {d} = svgInfo.children[0].attributes
+	const [, , width, height] = svgInfo.attributes.viewBox.split(' ').map(_ => parseInt(_))
+
+	const contours = joinWith(
+		svgToContours({width, height, d}),
+		contour => `
+		<contour>
+			${joinWith(contour.points, pt => `
+				<pt x="${pt.x}" y="${pt.y}" on="${Number(pt.onCurve)}" />`)}
+		</contour>
+	`)
+
+	const ttglyph = `
+	<TTGlyph name="${name}" xMin="0" yMin="0" xMax="${width}" yMax="${height}">
+	${contours}
+	<instructions />
+	</TTGlyph>
+`
+
+	const lines = br + results.join(br) + br
+	const tmpFile = `${tmpDir}/text-security-${name}.ttx`
+	const distFile = `${distDir}/text-security-${name}.woff`
+
+	// Take template.ttx and replace placehodlers with actual values.
+	const ttxFile = (await readFile('./template.ttx', 'utf8'))
+		.replace(/{{cmap}}/g, lines)
+		.replace(/{{ttglyph}}/g, ttglyph)
+		.replace(/{{name}}/g, name)
+		.replace(/{{width}}/g, width)
+	
+	// Generate intermediate ttx file
+	await writeFile(tmpFile, ttxFile)
+	// Generate woff file from the ttx
+	await spawn('ttx', ['-f', '-o', distFile, '--flavor', 'woff', tmpFile])
+})
+
+Promise.all(promises)
+	.then(() => readFile('./style-template.css', 'utf8'))
+	.then(styleTemplate => {
+		const css = names.map(name => styleTemplate.replace(/{{name}}/g, name)).join(br)
+		return writeFile('./bin/text-security.css', css)
+	})
+	.then(
+		() => console.log('Done!'),
+		() => console.error('Build failed.'))
+
+
+/**
+ * Creates a list of "contour" objects that are later
+ * used for the TTGlyph section of the ttx file, wich
+ * describes the actual shape of the glyph.
+ * 
+ * This function is inspired by / taken from a script by Vitaly Puzrin and Sergey Batishchev
+ * https://github.com/fontello/svg2ttf/blob/c33a126920f46b030e8ce960cc7a0e38a6946bbc/index.js
+ * and modified to better fit the code style of this script.
+ * 
+ * Original license:
+ * MIT https://github.com/fontello/svg2ttf/blob/c33a126920f46b030e8ce960cc7a0e38a6946bbc/LICENSE
+ * 
+ * Copyright for the original source: Vitaly Puzrin
+ * Author of the original source: Sergey Batishchev
+ *
+ */
+function svgToContours({width, height, d}) {
+	const accuracy = Math.max(width, height) * 0.0006
+	const svgPath = new SvgPath(d)
+		.abs()
+		.unshort()
+		.unarc()
+		.iterate((segment, index, x, y) => svg.cubicToQuad(segment, index, x, y, accuracy))
+
+	const sfntContours = svg.toSfntCoutours(svgPath)
+
+	return sfntContours.map(sfntContour => {
+		const contour = new sfnt.Contour()
+
+		contour.points = sfntContour.map(sfntPoint => {
+			const point = new sfnt.Point()
+
+			point.x = sfntPoint.x
+			point.y = sfntPoint.y
+			point.onCurve = sfntPoint.onCurve
+			return point
+		})
+
+		return contour
+	})
+}
+
+function spawn(cmd, args) {
+	return new Promise((resolve, reject) => {
+		childProcess.spawn(cmd, args, {stdio: 'inherit'})
+			.on('exit', _ => _ === 0 ? resolve() : reject(_))
+	})
+}
