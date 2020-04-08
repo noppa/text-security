2285d7a19bd4ca3ecb6ede9fbf55a973b8647bf9
Oskari Noppa <oskari@arpakuut.io>
2020-04-09 00:13:38 +0300

    Dynamically generate for all shapes

--- src/generate-fonts.py
+++ src/generate-fonts.py
@@ -2,17 +2,22 @@ import os
 import subprocess
 from pathlib import Path
 
-Path("/build-artifacts").mkdir(exist_ok=True)
-
 # /shapes directory should be "bind mounted" from host when creating the container.
 shape_files = os.listdir("/src/shapes")
+config_files = os.listdir("/src/font-config")
 font_template = Path("/src/font-template.ps").read_text()
-# cidfontinfo = Path("/workdir/cidfontinfo").read_text()
 
 for shape_file in shape_files:
   (shape_name, ext) = os.path.splitext(shape_file)
-  if ext != ".ps": continue
+  # Replace <<SPACE>> placeholder in the font template with shape definition.
   shape = Path(f"/src/shapes/{shape_file}").read_text()
-  font_template_compiled = font_template.replace('<<SHAPE>>', shape)
-  Path(f"/build-artifacts/font-{shape_name}.txt").write_text(font_template_compiled)
-  subprocess.call(["/src/generate-fonts.sh"])
+  font_name = f"text-security-{shape_name}"
+  font_template_compiled = font_template.replace("<<SHAPE>>", shape).replace("text-security", font_name)
+  Path(f"/tmp/{shape_name}-font.txt").write_text(font_template_compiled)
+
+  # Replace font name in font configuration files
+  for config_file in config_files:
+    config = Path(f"/src/font-config/{config_file}").read_text()
+    config_with_corrected_name = config.replace("text-security", font_name)
+    Path(f"/tmp/{shape_name}-{config_file}").write_text(config_with_corrected_name)
+    subprocess.call(["/src/generate-fonts.sh", shape_name])
--- src/generate-fonts.sh
+++ src/generate-fonts.sh
@@ -1,20 +1,22 @@
 #!/bin/sh
-
 set -e
-/t1utils/t1asm /build-artifacts/font-circle.txt > /build-artifacts/font.pfa
-mergefonts -cid /src/cidfontinfo /build-artifacts/cidfont.ps /build-artifacts/font.pfa
-makeotf -f /build-artifacts/cidfont.ps -omitMacNames \
-  -ff /src/features -fi /src/cidfontinfo \
-  -mf /src/FontMenuNameDB -r \
+
+shape=$1
+
+/t1utils/t1asm /tmp/$shape-font.txt > /tmp/$shape-font.pfa
+mergefonts -cid /tmp/$shape-cidfontinfo /tmp/$shape-cidfont /tmp/$shape-font.pfa
+makeotf -f /tmp/$shape-cidfont -omitMacNames \
+  -ff /tmp/$shape-features -fi /tmp/$shape-cidfontinfo \
+  -mf /tmp/$shape-FontMenuNameDB -r \
   -ch /adobe-blank-2/UnicodeAll-UTF32-H
 
 sfntedit \
   -a DSIG=/adobe-blank-2/DSIG.bin,OS/2=/adobe-blank-2/OS2.bin,cmap=/adobe-blank-2/cmap.bin \
-  -d VORG,vhea,vmtx /build-artifacts/text-security.otf
+  -d VORG,vhea,vmtx /tmp/text-security-$shape.otf
 
-sfntedit -f /build-artifacts/text-security.otf
+sfntedit -f /tmp/text-security-$shape.otf
 
-rm -f /build-artifacts/text-security.ttf
-otf2ttf /build-artifacts/text-security.otf
-sfntedit -d DSIG /build-artifacts/text-security.ttf
-mv /build-artifacts/text-security.ttf /output/
+rm -f /tmp/text-security-$shape.ttf
+otf2ttf /tmp/text-security-$shape.otf
+sfntedit -d DSIG /tmp/text-security-$shape.ttf
+mv /tmp/text-security-$shape.ttf /output/
