52e10d5ff1628902ed097922f28c2ecadba0d4c3
Oskari Noppa <oskari@arpakuut.io>
2020-04-09 00:50:10 +0300

    Add compat fonts

--- src/generate-fonts.sh
+++ src/generate-fonts.sh
@@ -20,3 +20,24 @@ rm -f /tmp/text-security-$shape.ttf
 otf2ttf /tmp/text-security-$shape.otf
 sfntedit -d DSIG /tmp/text-security-$shape.ttf
 mv /tmp/text-security-$shape.ttf /output/
+
+
+# Compatibility fonts for browsers that don't support cmap subtable 12.
+/t1utils/t1asm /tmp/$shape-compat-font.txt > /tmp/$shape-compat-font.pfa
+mergefonts -cid /tmp/$shape-compat-cidfontinfo /tmp/$shape-compat-cidfont /tmp/$shape-compat-font.pfa
+makeotf -f /tmp/$shape-compat-cidfont -omitMacNames \
+  -ff /tmp/$shape-compat-features -fi /tmp/$shape-compat-cidfontinfo \
+  -mf /tmp/$shape-compat-FontMenuNameDB -r \
+  -stubCmap4
+  -ch /adobe-blank-2/UnicodeAll-UTF32-H
+
+sfntedit \
+  -a DSIG=/adobe-blank/DSIG.bin
+  -d VORG,vhea,vmtx /tmp/text-security-$shape-compat.otf
+
+sfntedit -f /tmp/text-security-$shape-compat.otf
+
+rm -f /tmp/text-security-$shape-compat.ttf
+otf2ttf /tmp/text-security-$shape-compat.otf
+sfntedit -d DSIG /tmp/text-security-$shape-compat.ttf
+mv /tmp/text-security-$shape-compat.ttf /output/
