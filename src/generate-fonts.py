9422c1d25f3e729a3eaa7aa484894d6b564c60c0
Oskari Noppa <oskari@arpakuut.io>
2020-04-10 16:25:07 +0300

    Generate css file for each font
    The end user can save ~400 b by importing just the css for their
    specific shape if they don't need the others, yay.

--- src/generate-fonts.py
+++ src/generate-fonts.py
@@ -17,8 +17,6 @@ config_files = os.listdir("/src/font-config")
 font_template = Path("/src/font-template.ps").read_text()
 css_template = Path("/src/style-template.css").read_text()
 
-css_file_contents = []
-
 def get_shape_file_path(shape_file):
   return f"/src/shapes/{shape_file}"
 
@@ -28,32 +26,32 @@ def get_shape_mtime(shape_file):
 def get_shape_name(shape_file):
   return os.path.splitext(shape_file)[0]
 
-def prepare_font_config_file(shape_file):
+def generate_font_for_shape(shape_file, skip_compatibility_fonts=False):
   shape_name = get_shape_name(shape_file)
   shape = Path(get_shape_file_path(shape_file)).read_text()
   font_name = f"text-security-{shape_name}"
+  # Replace <<SPACE>> placeholder in the font template with shape definition.
   font_template_compiled = font_template.replace("<<SHAPE>>", shape).replace("text-security", font_name)
   Path(f"/tmp/{shape_name}-font.txt").write_text(font_template_compiled)
-
-def generate_font_for_shape(shape_name, skip_compatibility_fonts=False):
   extra_arg = "--no-compat" if skip_compatibility_fonts else ""
   subprocess.call(["/src/generate-fonts.sh", shape_name, extra_arg])
 
 def build_all():
+  css_file_contents = []
+
   for shape_file in shape_files:
     shape_name = get_shape_name(shape_file)
     font_name = f"text-security-{shape_name}"
-    prepare_font_config_file(shape_file)
-    # Replace <<SPACE>> placeholder in the font template with shape definition.
     shape_css = css_template.replace("text-security", f"text-security-{shape_name}")
     css_file_contents.append(shape_css)
+    Path(f"/output/text-security-{shape_name}.css").write_text(cssmin.cssmin(shape_css))
     # Replace font name in font configuration files
     for config_file in config_files:
       config = Path(f"/src/font-config/{config_file}").read_text()
       config_with_corrected_name = config.replace("text-security", font_name)
       Path(f"/tmp/{shape_name}-{config_file}").write_text(config_with_corrected_name)
     
-    generate_font_for_shape(shape_name)
+    generate_font_for_shape(shape_file)
 
   Path("/output/text-security.css").write_text(cssmin.cssmin("\n".join(css_file_contents)))
 
@@ -69,8 +67,7 @@ def watch():
       mtime = get_shape_mtime(shape_file)
       if mtime != file_mtimes[shape_file]:
         file_mtimes[shape_file] = mtime
-        prepare_font_config_file(shape_file)
-        generate_font_for_shape(get_shape_name(shape_file), True)
+        generate_font_for_shape(shape_file, True)
         
 build_all()
 if watch_mode:
